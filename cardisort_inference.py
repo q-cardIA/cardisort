392dd8b869a473ac98821e0841b8612804b3ca5f
Cian Scannell <cianmscannell@gmail.com>
2021-07-28 16:19:57 +0100

    add inference scripts

---
+++ cardisort_inference.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pydicom
+import sys
+import pickle
+import tensorflow.keras as tfk
+from skimage.transform import resize
+
+import cardisort_utils
+
+def main(args):
+
+    if len(args) < 2:
+        print("Directories not specified.")
+        return 1
+    if len(args) == 2:
+        input_dir = args[1]
+        output_dir = f"CardiSorted_{input_dir}"
+    if len(args) == 3:
+        input_dir = args[1]
+        output_dir = args[2]
+
+    M = 256
+    N = 256
+    cardisort_model = tfk.models.load_model('BestRun4-Ep31-9738-9863-0.13073.ckpt')
+    pickle_in = open("seq_categories.pickle","rb")
+    seq_categories = pickle.load(pickle_in)
+    pickle_in = open("plane_categories.pickle","rb")
+    plane_categories = pickle.load(pickle_in)
+
+    all_files = cardisort_utils.get_files(input_dir)
+
+    dicom_ds = pydicom.read_file(all_files[0])
+    the_manufacturer = dicom_ds.Manufacturer.lower()
+    if 'philips' in the_manufacturer or 'ge' in the_manufacturer:
+        this_order_patient_dict = cardisort_utils.get_philips_ge_dict(all_files)
+    elif 'siemens' in the_manufacturer:
+        this_order_patient_dict = cardisort_utils.get_siemens_dict(all_files)
+    else:
+        raise ValueError
+
+    this_patient_dict_for_infer, second_files = cardisort_utils.get_inference_dict(this_order_patient_dict, M, N)
+    series_counts = np.zeros((len(seq_categories),len(plane_categories)))
+    for key in this_patient_dict_for_infer:
+        out = cardisort_model(this_patient_dict_for_infer[key][np.newaxis,...])
+        s = np.argmax(out[0].numpy())
+        p = np.argmax(out[1].numpy())
+        series_counts[s,p] += 1
+        cardisort_utils.write_sorted(this_order_patient_dict[key],output_dir,seq_categories[s],plane_categories[p],series_counts[s,p])
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+    sys.exit(0)    
---
+++ cardisort_utils.py
@@ -0,0 +1,119 @@
+import os 
+import numpy as np
+import pydicom
+import cv2
+
+def write_sorted(list_of_files, out_fold, seq, plane, series_no):
+
+    this_seq_fold_series = os.path.join(out_fold, f"{seq}_{plane}", f"series{int(series_no)}")
+    if not os.path.exists(this_seq_fold_series):
+        os.makedirs(this_seq_fold_series)
+    for ii,file in enumerate(list_of_files):
+        num = f"0000{ii}"
+        file.save_as(os.path.join(this_seq_fold_series, f"{num[-4:]}.dcm"))
+
+def get_files(the_dir):
+
+    all_files = []
+    all_patient_items = os.listdir(the_dir)
+    all_patient_items = [os.path.join(the_dir, p) for p in all_patient_items]
+    for itm in all_patient_items:
+        if os.path.isfile(itm) and 'dicomdir' not in itm.lower():
+                all_files.append(itm)
+        if os.path.isdir(itm):
+            series_dir_files = os.listdir(itm)
+            series_dir_files = [os.path.join(itm,x) for x in series_dir_files]
+            for sub_itm in series_dir_files:
+                if os.path.isfile(sub_itm) and 'dicomdir' not in sub_itm.lower():
+                    all_files.append(sub_itm)
+    return all_files
+    
+def get_inference_dict(patient_dict, M, N):
+    inference_dict = {}
+    secondary_files = []
+    for key, this_series in patient_dict.items():
+        if 'secondary' not in this_series[0].SeriesDescription.lower():
+            
+            this_series_ims = selected_images(this_series)
+            im1 = this_series_ims[0].pixel_array
+            im1 = cv2.resize(im1, dsize=(M, N), interpolation=cv2.INTER_LINEAR)
+            if len(this_series_ims) == 1:
+                im2 = im1.copy()
+                im3 = im1.copy()
+            else:
+                im2 = this_series_ims[1].pixel_array
+                im2 = cv2.resize(im2, dsize=(M, N), interpolation=cv2.INTER_LINEAR)
+                if len(this_series_ims) == 2:
+                    im3 = im1.copy()
+                else:
+                    im3 = this_series_ims[2].pixel_array
+                    im3 = cv2.resize(im3, dsize=(M, N), interpolation=cv2.INTER_LINEAR) 
+
+            normim1 =  ( im1 - np.min(im1) ) / ( np.max(im1) - np.amin(im1) ).astype('float32')
+            normim2 =  ( im2 - np.min(im2) ) / ( np.max(im2) - np.amin(im2) ).astype('float32')
+            normim3 =  ( im3 - np.min(im3) ) / ( np.max(im3) - np.amin(im3) ).astype('float32')
+            normalised_newim = np.stack((normim1, normim2, normim3), axis=-1)
+            inference_dict[key] = normalised_newim 
+        else:
+            secondary_files.append(this_series)
+
+    return inference_dict, secondary_files
+
+
+def get_philips_ge_dict(files):
+
+    this_patient_dict = {}
+    instance_nums = {}
+    for file in files:
+        dicom_ds = pydicom.read_file(file)
+        this_patient_dict.setdefault(dicom_ds.SeriesInstanceUID, []).append(dicom_ds)
+        instance_nums.setdefault(dicom_ds.SeriesInstanceUID, []).append(int(dicom_ds.InstanceNumber))
+
+    return arrange_by_instance_philips_ge(this_patient_dict, instance_nums)
+
+def arrange_by_instance_philips_ge(patient_dict, all_instances):
+
+    ordered_patient_dict = {}
+    for key, ims in patient_dict.items():
+        the_instance_nums = all_instances[key]
+        the_order = np.argsort(the_instance_nums)
+        new_ims = []
+        for ii in the_order:
+            new_ims.append(ims[ii])
+        ordered_patient_dict[key] = new_ims
+    return ordered_patient_dict
+
+def get_siemens_dict(files):
+
+    this_patient_dict = {}
+    info_for_order = {}
+    for file in files:
+        dicom_ds = pydicom.read_file(file)
+        this_patient_dict.setdefault(dicom_ds.ProtocolName, []).append(dicom_ds)
+        info_for_order.setdefault(dicom_ds.ProtocolName, []).append([float(dicom_ds.SeriesTime),dicom_ds.SeriesInstanceUID,int(dicom_ds.InstanceNumber)])
+
+    return arrange_by_instance_siemens(this_patient_dict,info_for_order)
+
+def arrange_by_instance_siemens(patient_dict, instance_nums):
+
+    ordered_patient_dict = {}
+    for key, ims in patient_dict.items():
+        instances = instance_nums[key]
+        the_order = sorted(range(len(instances)), key=lambda k: instances[k]) 
+        new_ims = []
+        for ii in the_order:
+            new_ims.append(ims[ii])
+        ordered_patient_dict[key] = new_ims
+    return ordered_patient_dict
+
+
+def selected_images(input_list):
+    # if our list is empty return nan, have to see whether this works in our case
+    if 0 == len(input_list):
+        return np.nan
+    # if our list has just two elements we want to avoid to use the first one
+    if 0 < len(input_list) < 4:
+        return input_list
+    # get the index of the middle
+    middle = (len(input_list)//2)
+    return input_list[0], input_list[middle], input_list[-1]
